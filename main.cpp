b9725530a174fa834f24e5873687b762bb86fc09
franciscomcsousa <francisco.m.c.sousa@gmail.com>
2021-04-01 03:07:02 +0100

    fresh restart

--- main.cpp
+++ main.cpp
@@ -5,83 +5,95 @@
 
 using namespace std;
 
+// In color
+// "b" stands for "black"
+// "g" stands for "grey"
+// "w" stands for "white"
+
+typedef struct nodestr{
+    vector<int> adj;
+    char color;
+    int pi;
+    int number;
+} Node;
+
 vector<vector<int>> graph;
 
-vector<int> parents;
+vector<Node> dfsGraph;
+
+vector<int> sources;
 
 int maxNumber = 1;
 
-void readGraph(){
-    int n,m;
-    scanf("%d %d", &n, &m);
-    graph = vector<vector<int>>(n+1, vector<int>());
-    for(int i=0; i<m; i++){
-        int u,v;
-        scanf("%d %d", &u, &v);
-        graph[u].push_back(v);        
-    }
-}
-
-int minPieces(vector<vector<int>> graph){
-    vector<int> v = vector<int>(graph.size());
-    parents = vector<int>(graph.size());
-
+int readGraph()
+{
+    int n, m;
     int counter = 0;
 
-    for (int i = 1; i < graph.size(); i++)
+    scanf("%d %d", &n, &m);
+
+    graph = vector<vector<int>>(n, vector<int>());
+    vector<int> isSource = vector<int>(n, 1);
+
+    for(int i = 0; i < m; i++)
     {
-        for (int j = 0; j < graph[i].size(); j++)
-        {
-            v[graph[i][j]] = 1;
-        }
+        int u, v;
+        scanf("%d %d", &u, &v);
+        graph[u - 1].push_back(v);
+        isSource[v - 1] = 0;
     }
-    
-    for (int i = 1; i < v.size(); i++)
+    for (int i = 0; i < n; i++)
     {
-        if(v[i]==0)
+        if (isSource[i] == 1)
         {
-            parents.push_back(i);
+            sources.push_back(i + 1);
             counter++;
-            printf("%d\n", i);
         }
+    }
+    return counter;
+}
+
+void DFSVisit(int i, int counter)
+{
+    dfsGraph[i].color = 'g';
+
+    printf("counter: %d\n", counter);
+
+    for (int j = 0; j < dfsGraph[i].adj.size(); j++)
+    {
+        Node node = dfsGraph[dfsGraph[i].adj[j] - 1];
+        if (node.color == 'w')
+        {
+            node.pi = i + 1;
+            counter++;
+            DFSVisit(node.number, counter);
+        }
+    }
+}
+
+void DFS(int n)
+{
+    dfsGraph = vector<Node>(graph.size());
+    for (int i = 0; i < graph.size(); i++)
+    {
+        dfsGraph[i].color = 'w';
+        dfsGraph[i].pi = -1;
+        dfsGraph[i].adj = graph[i];
+        dfsGraph[i].number = i + 1;
     }
     
-    return counter;
-
-}
-
-void maxSequence(int parent, int counter){
-
-    int maxProgress = counter;
-    maxProgress++;
-
-    if(graph[parent].size() == 0){
-        if(maxProgress > maxNumber){
-            maxNumber = maxProgress;
-        }
-    }
-    else{
-        for(int i = 0; i < graph[parent].size(); i++){
-            maxSequence(graph[parent][i], maxProgress);
-        }
+    if(dfsGraph[n].color == 'w')
+    {
+        DFSVisit(n, 0);
     }
 }
 
-void computeParents(){
-    for(int i = 0; i < parents.size(); i++){
-        maxSequence(graph, parents[i], 0);
-    }
-}
+int main()
+{
 
-int main(){
+    int min = readGraph();
 
-    readGraph();
-
-    int min = minPieces(graph);
-
-    computeParents();
-
-    printf("%d %d\n", min, maxNumber);
+    DFS(3);
 
     return 0;
 }
